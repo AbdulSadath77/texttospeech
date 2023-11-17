7a717b3a44f20442d33de9c3ead4150066f13a85
AbdulSadath77 <127017378+AbdulSadath77@users.noreply.github.com>
2023-11-17 18:31:10 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,93 @@
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+// DOM Elements
+const textForm = document.querySelector("form");
+const textInput = document.getElementById("floatingTextarea");
+const voiceSelect = document.getElementById("voice-select");
+const rate = document.getElementById("rate");
+const rateValue = document.getElementById("rate-value");
+const pitch = document.getElementById("pitch");
+const pitchValue = document.getElementById("pitch-value");
+
+// Init voices array
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+  //   console.log(voices);
+  //   Loop through voices and create an opetion for each one
+  voices.forEach((voice) => {
+    // Create option element
+    const option = document.createElement("option");
+    // Fill option with voice and language
+    option.innerHTML = `${voice.name}(${voice.lang})`;
+
+    // Set needed option attributes
+    option.setAttribute("data-name", voice.name);
+    option.setAttribute("data-lang", voice.lang);
+    voiceSelect.appendChild(option);
+  });
+};
+
+getVoices();
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+
+// Speak
+const speak = () => {
+  // Check if speaking
+  if (synth.speaking) {
+    console.error("Already speaking...");
+    return;
+  }
+  if (textInput.value !== "") {
+    // Get speak text
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+
+    // Speak end
+    speakText.onend = (e) => {
+      console.log("Done speaking...");
+    };
+
+    // Speak error
+    speakText.onerror = (e) => {
+      console.error("Something went wront!");
+    };
+
+    // Selected voice
+    const selectedVoice =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+
+    // Loop through voices
+    voices.forEach((voice) => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+
+    // Set pitch and rate
+    speakText.rate = rate.value;
+    // Set pitch and pitch
+    speakText.pitch = pitch.value;
+    // Speak
+    synth.speak(speakText);
+  }
+};
+
+// EVENT LISTENERS
+
+// Text form submit
+textForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  speak();
+  textInput.blur();
+});
+
+// Rate value change
+rate.addEventListener("change", (e) => (rateValue.textContent = rate.value));
+// Pitch value change
+pitch.addEventListener("change", (e) => (pitchValue.textContent = pitch.value));
+// Voice select change
+voiceSelect.addEventListener("change", (e) => speak());
